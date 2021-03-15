60f5ac06750b784d52ce6f31cc3c598138c11b2b
Taylor Sun <Taylor_Sun@epam.com>
2021-03-15 11:28:15 +0800

    task1: modify issues found by code review

--- src/task_1/task_1.js
+++ src/task_1/task_1.js
@@ -1,18 +1,18 @@
 "use strict";
 
+const readline = require("readline");
+
 const revertInput = () => {
-    process.stdin.resume();
-    process.stdin.setEncoding("utf8");
+    const rl = readline.createInterface({
+        input: process.stdin,
+        output: process.stdout,
+        terminal: false
+    });
 
-    process.stdin.on("readable", () => {
-        let data
-        while (data = process.stdin.read()) {
-            // remove ending chars \r\n
-            data = data.replace(/(\r|\n|\r\n|\n\r)$/, "");
-
-            const outputStr = data.split("").reverse().join("")
-
-            process.stdout.write(`${outputStr}\r\n\r\n`);
+    rl.on("line", (data) => {
+        if (data) {
+            console.log(data.split("").reverse().join(""));
+            console.log();
         }
     });
 }
--- src/task_1/task_2.js
+++ src/task_1/task_2.js
@@ -6,9 +6,9 @@ const fs = require("fs");
 const csvFilePath = path.join(__dirname, "./data.csv");
 const resultFilePath = path.join(__dirname, "./results.json");
 
-const csvToJsonFile = async () => {
+const csvToJsonFile = () => {
     try {
-        await fs.writeFileSync(resultFilePath, "", { "flag": "w" });
+        fs.writeFileSync(resultFilePath, "", { "flag": "w" });
 
         const readStream = fs.createReadStream(csvFilePath);
         const writeStream = fs.createWriteStream(resultFilePath);
